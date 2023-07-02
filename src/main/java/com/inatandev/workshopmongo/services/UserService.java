a47ff30ded88c3234a89329aaa22bc245ab97c2b
INatanDev <natan.ferreira21@hotmail.com>
2023-07-02 15:47:02 -0300

    Obtendo um usuario por id

--- src/main/java/com/inatandev/workshopmongo/services/UserService.java
+++ src/main/java/com/inatandev/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.inatandev.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.inatandev.workshopmongo.domain.User;
 import com.inatandev.workshopmongo.repository.UserRepository;
+import com.inatandev.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,10 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
