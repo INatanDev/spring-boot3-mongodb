7360944d88cbe3a0f4afce4106f0af003aab34bf
INatanDev <natan.ferreira21@hotmail.com>
2023-07-02 11:53:17 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/inatandev/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.inatandev.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.inatandev.workshopmongo.domain.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+
+	
+}
---
+++ src/main/java/com/inatandev/workshopmongo/services/UserService.java
@@ -0,0 +1,20 @@
+package com.inatandev.workshopmongo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.inatandev.workshopmongo.domain.User;
+import com.inatandev.workshopmongo.repository.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repo;
+	
+	public List<User> findAll(){
+		return repo.findAll();
+	}
+}
