b9446472c4eff5164bcfa11284959c41735199c1
INatanDev <natan.ferreira21@hotmail.com>
2023-07-03 16:32:18 -0300

    Consulta simples com query methods

--- src/main/java/com/inatandev/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/inatandev/workshopmongo/repository/PostRepository.java
@@ -1,5 +1,7 @@
 package com.inatandev.workshopmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +10,5 @@ import com.inatandev.workshopmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 
-	
+	List<Post> findByTitleContainingIgnoreCase(String text);
 }
