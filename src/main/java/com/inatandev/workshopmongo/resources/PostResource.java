3fcf06caba50c2e56c77c17ee42cf860a1a60986
INatanDev <natan.ferreira21@hotmail.com>
2023-07-03 16:59:43 -0300

    Consulta simples com @Query

--- src/main/java/com/inatandev/workshopmongo/resources/PostResource.java
+++ src/main/java/com/inatandev/workshopmongo/resources/PostResource.java
@@ -28,7 +28,7 @@ public class PostResource {
 	}
 	
 	@GetMapping(value = "/titlesearch")
-	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text){
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value="text", defaultValue="") String text) {
 		text = URL.decodeParam(text);
 		List<Post> list = service.findByTitle(text);
 		return ResponseEntity.ok().body(list);
--- src/main/java/com/inatandev/workshopmongo/services/PostService.java
+++ src/main/java/com/inatandev/workshopmongo/services/PostService.java
@@ -21,7 +21,7 @@ public class PostService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 	
-	public List<Post> findByTitle(String text){
-		return repo.findByTitleContainingIgnoreCase(text);
+	public List<Post> findByTitle(String text) {
+		return repo.searchTitle(text);
 	}
 }
